bc75f98111f4ee311e3349336287694526bc70ba
m.iken <mouadiken2@gmail.com>
2022-01-06 11:21:05 +0100

    first commit

---
+++ src/test/java/com/microservice/demo/demo/RegisterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
